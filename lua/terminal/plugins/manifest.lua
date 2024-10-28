dd4429c5729c4ee029c9c1153297270b74f87029
27Onion Nebell <zzy20080201@qq.com>
2024-10-28 13:53:50 +0800

    add fuckin' lazy git client

--- lua/terminal/plugins/manifest.lua
+++ lua/terminal/plugins/manifest.lua
@@ -114,6 +114,30 @@ return {
     },
     -- }}}
 
+    --{{{ LazyGit
+    {
+        "kdheepak/lazygit.nvim",
+        lazy = true,
+        cmd = {
+            "LazyGit",
+            "LazyGitConfig",
+            "LazyGitCurrentFile",
+            "LazyGitFilter",
+            "LazyGitFilterCurrentFile",
+        },
+        -- optional for floating window border decoration
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+        },
+        -- setting the keybinding for LazyGit with 'keys' is recommended in
+        -- order to load the plugin when the command is run for the first time
+        keys = {
+            { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
+        }
+    },
+    --}}}
+
+
     { "ryanoasis/vim-devicons", lazy = false },
     { "honza/vim-snippets", lazy = true },
     { "mhinz/vim-startify" },
