8b6d5e6b4b5133ad622487f53a64f0cb6b51c1b2
27Onion Nebell <zzy20080201@gmail.com>
2025-03-27 14:27:59 +0800

    strip debug print

--- lua/terminal/tweaks/csharp-lsp-correction.lua
+++ lua/terminal/tweaks/csharp-lsp-correction.lua
@@ -1,7 +1,6 @@
 local url = require("common.utils.url")
 
 local function virtual_text_document(params)
-    print(params.match:sub(1))
     local bufnr = params.buf
     local actual_path = params.match:sub(1)
 
@@ -13,7 +12,6 @@ local function virtual_text_document(params)
     local client = clients[1]
     local req_params = { timeout = 5000, textDocument = { uri = url.urldecode(actual_path) } }
     local result, err = client:request_sync("csharp/metadata", req_params, 10000, 0)
-    print(vim.inspect(result))
     if not err and result ~= nil then
         local lines = vim.split(result.result.source, "\n")
         vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, lines)
