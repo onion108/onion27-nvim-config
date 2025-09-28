555db7c64a0386b4ffc781284e3645dab08a2c80
27Onion Nebell <zzy20080201@gmail.com>
2025-09-28 11:59:55 +0800

    update

--- lua/common/load-lazy.lua
+++ lua/common/load-lazy.lua
@@ -1,5 +1,8 @@
 -- Bootstrap lazy.nvim (just grabbed from their website)
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if vim.loop then
+  vim.notify("YOU'RE TOO FUCKING OLD")
+end
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
   local out = vim.fn.system { "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath }
