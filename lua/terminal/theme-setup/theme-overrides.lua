c44b58fa6d2af2ca38048c1ba30be3e2556f7e75
27Onion Nebell <zzy20080201@gmail.com>
2025-11-25 05:07:16 +0800

    use mizuki color on startup

--- lua/terminal/theme-setup/theme-overrides.lua
+++ lua/terminal/theme-setup/theme-overrides.lua
@@ -3,4 +3,8 @@ return {
     local colors = require("yorumi.colors")
     vim.api.nvim_set_hl(0, "PmenuSel", { bg = colors.yoru3, fg = colors.tsuki4, bold = true })
   end,
+  ["*"] = function ()
+    vim.api.nvim_set_hl(0, "DashboardHeader", { fg = "#ddaacc" })
+  end
 }
+
