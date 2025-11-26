496ecfde505f92c4f9ada1112b9993b0dc284ca0
27Onion Nebell <zzy20080201@gmail.com>
2025-11-26 12:27:23 +0800

    fix visual

--- lua/terminal/theme-setup/theme-overrides.lua
+++ lua/terminal/theme-setup/theme-overrides.lua
@@ -2,6 +2,7 @@ return {
   yorumi = function()
     local colors = require("yorumi.colors")
     vim.api.nvim_set_hl(0, "PmenuSel", { bg = colors.yoru3, fg = colors.tsuki4, bold = true })
+    vim.api.nvim_set_hl(0, "Visual", { bg = colors.kuroiGray })
   end,
   ["*"] = function ()
     vim.api.nvim_set_hl(0, "DashboardHeader", { fg = "#ddaacc" })
