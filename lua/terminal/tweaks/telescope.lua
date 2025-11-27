452359f2a871590d1c71223b436ad646e8a2ed2c
27Onion Nebell <zzy20080201@gmail.com>
2025-11-28 03:35:11 +0800

    ADVANCED UPDATE

--- lua/terminal/tweaks/telescope.lua
+++ lua/terminal/tweaks/telescope.lua
@@ -1,7 +1,7 @@
 local keymap = require("common.utils.keymap")
 
 local success = pcall(function()
-  local telescope = require("telescope")
+  local themes = require("telescope.themes")
   local builtin = require("telescope.builtin")
   keymap.define_keymap(
     "n",
@@ -11,11 +11,18 @@ local success = pcall(function()
     { silent = true }
   )
   keymap.define_keymap("n", "<leader>gp", "<cmd>Telescope live_grep<cr>", "GREP using telescope", { silent = true })
-  keymap.define_keymap("n", "<leader>gf", "<cmd>Telescope find_files<cr>", "Find file", { silent = true })
+  keymap.define_keymap("n", "<leader>gf", function ()
+    builtin.find_files(themes.get_ivy())
+    end, "Find file", { silent = true })
   keymap.define_keymap("n", "<leader>gF", function()
-    builtin.find_files { hidden = true, no_ignore = true, no_ignore_parent = true }
+    builtin.find_files(themes.get_ivy({ hidden = true, no_ignore = true, no_ignore_parent = true }))
   end, "Find file", { silent = true })
-  keymap.define_keymap("n", "<leader>gb", "<cmd>Telescope buffers<cr>", "Find buffer", { silent = true })
+  keymap.define_keymap("n", "<leader>gb", function ()
+    builtin.buffers(themes.get_ivy())
+    end, "Find buffer", { silent = true })
+  keymap.define_keymap("n", "<leader>gd", function ()
+    builtin.diagnostics(themes.get_ivy())
+    end, "Telescope Diagnostics", { silent = true })
 end)
 
 if not success then
