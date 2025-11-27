847ecf6796f4bb320edf59aad1ab9bd04eca6951
27Onion Nebell <zzy20080201@gmail.com>
2025-11-28 03:37:00 +0800

    oh poor style

--- lua/terminal/theme-setup/theme-overrides.lua
+++ lua/terminal/theme-setup/theme-overrides.lua
@@ -4,8 +4,7 @@ return {
     vim.api.nvim_set_hl(0, "PmenuSel", { bg = colors.yoru3, fg = colors.tsuki4, bold = true })
     vim.api.nvim_set_hl(0, "Visual", { bg = colors.kuroiGray })
   end,
-  ["*"] = function ()
+  ["*"] = function()
     vim.api.nvim_set_hl(0, "DashboardHeader", { fg = "#ddaacc" })
-  end
+  end,
 }
-
