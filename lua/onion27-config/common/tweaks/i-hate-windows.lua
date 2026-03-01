4f76d036d1c212556119ed6913b3fbc6953faeb0
27Onion Nebell <zzy20080201@gmail.com>
2026-03-01 18:14:19 +0800

    more fucking windows

--- lua/onion27-config/common/tweaks/i-hate-windows.lua
+++ lua/onion27-config/common/tweaks/i-hate-windows.lua
@@ -2,6 +2,21 @@ if vim.fn.has("win32") == 1 or vim.fn.has("win64") == 1 then
   vim.api.nvim_echo({
     {
       "No. We won't allow this configuration to run on this fucking shitty and awful operating system. Use Linux or macOS if you want to run.",
+      "ErrorMsg",
+    },
+  }, true, {})
+  os.exit(69)
+end
+
+if vim.fn.has("wsl") == 1 then
+  vim.api.nvim_echo({
+    {
+      "Even if you're using WSL doesn't hide the fact that you're using the fucking shitty and awful niche garbage operating system.",
+      "ErrorMsg",
+    },
+    {
+      "Get out of my configuration. Use a proper operating system.",
+      "ErrorMsg",
     },
   }, true, {})
   os.exit(69)
