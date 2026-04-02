8def4918350312baf8edc9774efe66e9b3178539
27Onion Nebell <zzy20080201@gmail.com>
2026-04-03 00:17:37 +0800

    use 0.13

--- lua/onion27-config/postcommon/main.lua
+++ lua/onion27-config/postcommon/main.lua
@@ -1,4 +1,4 @@
-if vim.fn.has("nvim-0.12") ~= 1 then
+if vim.fn.has("nvim-0.13") ~= 1 then
   vim.notify(
     "Your nvim is too fucking old but we decided to spare you because you fucking ass don't know how to upgrade to the fucking nightly. We don't guarantee this shit will fucking work on your poor fucking old nvim instance! Good luck.",
     vim.log.levels.WARN
